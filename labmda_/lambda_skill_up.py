ec35c9940dc148d0dd1905252af207b7121a478d
spidyweb <jiho358896@gmail.com>
2022-03-04 13:36:50 +0900

    lambda updated

--- labmda_/lambda_skill_up.py
+++ labmda_/lambda_skill_up.py
@@ -8,7 +8,16 @@ print(sorted(target, key=my_key))
 #labmda로 표현
 print(sorted(target, key=lambda x : len(x.strip())))
 
-#map 함수
+#map 함수 1
 my_list = [1,2,3,4]
 a = map(lambda x : x + 1,my_list)
 print(list(a))
+
+#map 함수 예제 2
+my_list2 = ["a", "b", "c", "d"]
+b = map(lambda y: y.upper(), my_list2)
+print(list(b))
+
+#filter 함수 예제
+result = list(filter((lambda x: x % 2 == 1), range(20)))
+print(result)
