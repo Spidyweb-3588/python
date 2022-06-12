16282d6f5dc7cb90352576bbca8007d519eebe3e
spidyweb <jiho358896@gmail.com>
2022-06-12 11:42:34 +0900

    bs4 2022-06-12 updated

--- bs4_/bs4_shoppingmall.py
+++ bs4_/bs4_shoppingmall.py
@@ -37,16 +37,23 @@ def word_boom_style():
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "lxml")
 
-    rank100_boom_style = soup.select(".item-wrap  .item-cont  .item-list .prd-name a")
+    rank100_boom_style_all = soup.select(".item-wrap  .item-cont  .item-list .prd-name a")
+    rank100_boom_style_font = soup.select(".item-wrap  .item-cont  .item-list .prd-name a font")
+    if rank100_boom_style_font[0].text in rank100_boom_style_all[0].text:
+        print(rank100_boom_style_all[0].text.replace(rank100_boom_style_font[0].text,""))
+
+    """rank100_boom_style_list = [i.text[0] for i in rank100_boom_style]
+    print(rank100_boom_style_list)
+
     rank100_boom_style_list = [i.text for i in rank100_boom_style]
     rank100_boom_style_list =\
     [x.replace("\r","").replace("\n","").replace("\t","").replace("(","").replace(")","") for x in rank100_boom_style_list]
 
     rank100_boom_style_list_word_list =\
-    [y.replace(" ","") for x in rank100_boom_style_list for y in x.split(" ") if y != ""]
+    [y.replace(" ","") for x in rank100_boom_style_list for y in x.split(" ") if y != ""]"""
 
-    print(rank100_boom_style_list)
-    return rank100_boom_style_list_word_list
+    #print(rank100_boom_style_list)
+    #return rank100_boom_style_list_word_list
 
 if __name__=="__main__":
     word_musinsa()
